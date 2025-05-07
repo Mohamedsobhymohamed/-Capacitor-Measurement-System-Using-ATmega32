03c572d2687316e5cd4c9fe3494064f0d57e3284
Mohamed Sobhy Mohamed saker <112513094+Mohamedsobhymohamed@users.noreply.github.com>
2025-05-07 16:23:45 +0300

    Add files via upload

---
+++ lcd.h
@@ -0,0 +1,11 @@
+#ifndef LCD_H
+#define LCD_H
+
+
+void lcd_init(void);
+void lcd_string(char *p, unsigned char pos); 
+void lcd_clear(void);
+void hex2ascii(char *target, long value, char pointplace);
+char long2ascii(char *target, unsigned long value);
+
+#endif
